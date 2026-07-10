2bfad5eb356180f43206fcaab10445af1371dd7a
pulicarpus <pulicarpus@gmail.com>
2026-07-10 11:51:11 +0700

    update lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,26 +5,49 @@ import 'tab_profil.dart';
 import 'tab_aktif.dart';
 import 'tab_pengaturan.dart';
 
-void main() => runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Voucher Master Pro',
+      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
+      home: const HomePage(),
+    );
+  }
+}
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  int _index = 0;
-  final List<Widget> _pages = [const TabCetak(), const TabVoucher(), const TabProfil(), const TabAktif(), const TabPengaturan()];
-  
+  int _currentIndex = 0;
+
+  // Tidak ada lagi passing parameter IP, User, Pass ke dalam List
+  final List<Widget> _pages = const [
+    TabCetak(),
+    TabVoucher(),
+    TabProfil(),
+    TabAktif(),
+    TabPengaturan(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _pages[_index],
+      body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
-        currentIndex: _index,
-        onTap: (i) => setState(() => _index = i),
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.print), label: 'Cetak'),
           BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Voucher'),
